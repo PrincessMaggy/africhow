e1a2fb3b6c8cdf48e565c7f534415a890c7b4d98
Maggy <edoziemagdalene@gmail.com>
2023-10-16 16:29:04 +0100

    setting up meal data

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState, useEffect} from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import fetchRestaurantData from './fetchData';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [data, setData] = useState([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    useEffect(() => {
+        fetchRestaurantData()
+            .then((result) => {
+                setData(result);
+            })
+            .catch((error) => {
+                console.error('Error fetching data:', error);
+            });
+    }, []);
+
+    return (
+        <>
+            <div>
+                <a href='https://vitejs.dev'>
+                    <img src={viteLogo} className='logo' alt='Vite logo' />
+                </a>
+                <a href='https://react.dev'>
+                    <img
+                        src={reactLogo}
+                        className='logo react'
+                        alt='React logo'
+                    />
+                </a>
+            </div>
+            <div className='card'>
+                <div>
+                    <ul>
+                        {data.map((menuItem, index) => (
+                            <div key={index}>
+                                <img src={menuItem.strCategoryThumb} />
+
+                                <p>Name: {menuItem.strCategory}</p>
+                                <p>
+                                    Description:{' '}
+                                    {menuItem.strCategoryDescription}
+                                </p>
+                                <p>Cost: ${menuItem.cost}</p>
+                                <p>Location: {menuItem.location}</p>
+                                <p>Status: {menuItem.status}</p>
+                            </div>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
---
+++ src/fetchData.jsx
@@ -0,0 +1,46 @@
+const fetchRestaurantData = async () => {
+    const apiUrl = 'https://www.themealdb.com/api/json/v1/1/categories.php';
+
+    try {
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+
+        const menuData = data.categories.map((menuItem) => ({
+            ...menuItem,
+            cost: getRandomCost(),
+            location: getRandomLocation(),
+            status: getRandomStatus(),
+        }));
+
+        return menuData;
+    } catch (error) {
+        console.error('Error:', error);
+    }
+};
+
+const getRandomCost = () => {
+    const minCost = 5;
+    const maxCost = 20;
+    return (Math.random() * (maxCost - minCost) + minCost).toFixed(2);
+};
+
+const getRandomLocation = () => {
+    const locations = [
+        'Downtown',
+        'Suburb',
+        'Countryside',
+        'Beachfront',
+        'Mountainside',
+    ];
+    return locations[Math.floor(Math.random() * locations.length)];
+};
+
+const getRandomStatus = () => {
+    const statuses = ['Available', 'Not Available'];
+    return statuses[Math.floor(Math.random() * statuses.length)];
+};
+
+export default fetchRestaurantData;
