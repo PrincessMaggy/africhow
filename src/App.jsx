9073307f490748ace449f28d30d475771f4a7cec
thisislaait <ogadi.ugo@gmail.com>
2023-10-19 01:51:44 +0200

    navlist data added

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,12 @@
-import {useState, useEffect} from 'react';
+import React from 'react';
 import './App.css';
-import fetchRestaurantData from './lib/fetchData';
-import EmailForm from './components/emailHandler';
+
 
 function App() {
-    const [data, setData] = useState([]);
-
-    useEffect(() => {
-        fetchRestaurantData()
-            .then((result) => {
-                setData(result);
-            })
-            .catch((error) => {
-                console.error('Error fetching data:', error);
-            });
-    }, []);
 
     return (
         <>
-            {/* Vendor Support */}
-            <h1>Vendor support</h1>
-            <EmailForm />
-            <div>
-                <h1>Meals</h1>
-            </div>
-            <div className='card'>
-                <div>
-                    <ul>
-                        {data.map((menuItem, index) => (
-                            <div key={index}>
-                                <img src={menuItem.strCategoryThumb} />
-
-                                <p>Name: {menuItem.strCategory}</p>
-                                <p>
-                                    Description:{' '}
-                                    {menuItem.strCategoryDescription}
-                                </p>
-                                <p>Cost: ${menuItem.cost}</p>
-                                <p>Location: {menuItem.location}</p>
-                                <p>Status: {menuItem.status}</p>
-                            </div>
-                        ))}
-                    </ul>
-                </div>
-            </div>
+        
         </>
     );
 }
---
+++ src/lib/navLists.js
@@ -0,0 +1,73 @@
+const navList = [
+    {
+      id: 1,
+      header: 'AfriChow',
+      items: [
+            {title: 'Join the trybe', route: '#' },
+            {title: 'Loyalty Rewards', route: '#' },
+            {title: 'KYC Onboarding', route: '#' },
+            {title: 'Support', route: '#' },
+      ]
+    },
+    {
+      id: 2,
+      header: 'Community',
+      items: [
+            {title: 'Join the trybe', route: '#' },
+            {title: 'Loyalty Rewards', route: '#' },
+            {title: 'KYC Onboarding', route: '#' },
+            {title: 'Support', route: '#' },
+      ]
+    },
+    {
+      id: 3,
+      header: 'Rewards',
+      items: [
+            {title: 'Join the trybe', route: '#' },
+            {title: 'Loyalty Rewards', route: '#' },
+            {title: 'KYC Onboarding', route: '#' },
+            {title: 'Support', route: '#' },
+      ]
+    },
+    {
+      id: 4,
+      header: 'Onboarding',
+      items: [
+            {title: 'Join the trybe', route: '#' },
+            {title: 'Loyalty Rewards', route: '#' },
+            {title: 'KYC Onboarding', route: '#' },
+            {title: 'Support', route: '#' },
+      ]
+    },
+    {
+      id: 4,
+      header: 'Support',
+      items: [
+            {title: 'Join the trybe', route: '#' },
+            {title: 'Loyalty Rewards', route: '#' },
+            {title: 'KYC Onboarding', route: '#' },
+            {title: 'Support', route: '#' },
+      ]
+    },
+    {
+      id: 5,
+      header: 'Links',
+      items: [
+            {title: 'About', route: '#' },
+            {title: 'Support', route: '#' },
+            {title: 'FAQs', route: '#' },
+      ]
+    },
+    {
+      id: 6,
+      header: 'Socials',
+      items: [
+            {title: 'Meta', route: '#' },
+            {title: 'LinkedIn', route: '#' },
+            {title: 'X (Twitter)', route: '#' },
+      ]
+    },
+    
+    ];
+    
+    export default navList;
