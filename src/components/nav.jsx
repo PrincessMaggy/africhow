36d928fff34bee4c4da0130f305fadf2cb1cad82
thisislaait <ogadi.ugo@gmail.com>
2023-10-19 14:46:52 +0200

    nav component set

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import HamBurger from "../assets/hamburger/white.svg";
 
 const nav = () => {
+
   return (
-    <div>this is the nav</div>
-  )
+    <div className='px-10 py-10 bg-green-200 text-white flex justify-between items-center top-0 right-0 left-0 z-10 bg-transparent'>
+      <div className='flex gap-3 justify-center items-center'>
+        <img src= {HamBurger} alt="" srcset="" className='w-9 cursor-pointer rotate-180 border-2 border-black p-2 bg-black' />
+        <h3 className='text-black text-lg'>AfriChow</h3>
+      </div>
+      <div className='flex md:justify-center'>
+        <button className='text-sm bg-[#33CC9F] rounded-sm bg-opacity-80 text-white py-1 px-5 font-black pointer'>
+          Login
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default nav
