13ff99e15d51c9674a4903e42d71a4996c7f9399
thisislaait <ogadi.ugo@gmail.com>
2023-10-19 17:20:52 +0200

    Footer created and added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Home from "./pages/Home"
 import './App.css';
+import Footer from './components/Footer'
 import Nav from "./components/nav"
 
 
@@ -12,6 +13,7 @@ function App() {
             <Routes>
                 <Route path='/' element={<Home />} />
             </Routes>
+            <Footer />
         </Router>
     );
 }
