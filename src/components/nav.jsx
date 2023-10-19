f27eeedc83c628293137e06cbf458f31db9a2003
thisislaait <ogadi.ugo@gmail.com>
2023-10-19 15:24:16 +0200

    dynamic navheader titles mapped per file path

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import HamBurger from "../assets/hamburger/white.svg";
+import HeaderTitle from "../mapping/navHeaderTitle"
 
 const nav = () => {
 
   return (
     <div className='px-10 py-10 bg-green-200 text-white flex justify-between items-center top-0 right-0 left-0 z-10 bg-transparent'>
       <div className='flex gap-3 justify-center items-center'>
-        <img src= {HamBurger} alt="" srcset="" className='w-9 cursor-pointer rotate-180 border-2 border-black p-2 bg-black' />
-        <h3 className='text-black text-lg'>AfriChow</h3>
+        <img src= {HamBurger} alt="" srcset="" className='w-8 cursor-pointer rotate-180 border-2 border-black p-2 bg-black' />
+        <HeaderTitle />
       </div>
       <div className='flex md:justify-center'>
         <button className='text-sm bg-[#33CC9F] rounded-sm bg-opacity-80 text-white py-1 px-5 font-black pointer'>
