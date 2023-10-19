edee84212661d057531cd8b03ba0b5db2194e701
thisislaait <ogadi.ugo@gmail.com>
2023-10-19 15:05:38 +0200

    app updated with route and home page created

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Home from "./pages/Home"
 import './App.css';
 import Nav from "./components/nav"
 
@@ -9,7 +10,7 @@ function App() {
         <Router>
             <Nav />
             <Routes>
-
+                <Route path='/' element={<Home />} />
             </Routes>
         </Router>
     );
