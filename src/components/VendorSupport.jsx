fdd281bba2f22da674ee4f31f7d7f811c8f327f4
Gloria Williams <willyabiodun@gmail.com>
2023-10-19 19:22:45 +0100

    responsivity for first page Vendor Support

--- src/components/VendorSupport.jsx
+++ src/components/VendorSupport.jsx
@@ -7,7 +7,6 @@ export default function VendorSupport() {
     return (
         <div>
             <Navbar />
-            <img src="./src/assets/arrow_back.png" className="mx-5"></img>
             <div className="mx-4 mt-4 font-body md:text-center">
                 <h1 className="font-bold text-3xl md:text-5xl md:mt-16 ">Help desk</h1>
                 <p className="py-2 text-xs font-normal md:text-lg">
