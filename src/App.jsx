c8306ffa86eb4a2eb21234d1a358680d3ff903b6
Opeyemi Esther Agbaje <damamy96@gmail.com>
2023-10-20 12:54:37 +0100

    Fixed imports and double nav issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,54 @@
 import {Route, Routes} from 'react-router-dom';
+import {useState} from 'react';
 import Home from './pages/Home';
 import './App.css';
 import Footer from './components/Footer';
 import Nav from './components/nav';
 import './index.css';
-import Rewards from './components/Rewards/Rewards';
-import Achievements from './components/Rewards/Achievements';
-import EarnStars from './components/Rewards/EarnStars';
-import Catalog from './components/Rewards/catalog/Catalog';
-import CatalogDetails from './components/Rewards/catalog/CatalogDetails';
-import VendorsDashboard from './components/vendorsPayment/vendorsDashboard';
-import VendorsTransaction from './components/vendorsPayment/vendorsTransaction';
-import StoreOverview from './components/storePerformance/StoreOverview';
-import StorePerformance from './components/storePerformance/StorePerformance';
+import Rewards from './components/Rewards/Rewards.jsx';
+import Achievements from './components/Rewards/Achievements.jsx';
+import EarnStars from './components/Rewards/EarnStars.jsx';
+import Catalog from './components/Rewards/catalog/Catalog.jsx';
+import CatalogDetails from './components/Rewards/catalog/CatalogDetails.jsx';
+import VendorsDashboard from './components/vendorsPayment/vendorsDashboard.jsx';
+import VendorsTransaction from './components/vendorsPayment/vendorsTransaction.jsx';
+import StoreOverview from './components/storePerformance/StoreOverview.jsx';
+import StorePerformance from './components/storePerformance/StorePerformance.jsx';
 
 function App() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+    const toggleDropdown = () => {
+        setMenuOpen(!menuOpen);
+    };
     return (
         <>
-            <Nav />
-                <Routes>
-                    <Route path='/' element={<Home />} />
-                    <Route path='/rewards' element={<Rewards />} />
-                    <Route path='/my-rewards' element={<Achievements />} />
-                    <Route path='/earn-stars' element={<EarnStars />} />
-                    <Route path='/catalog' element={<Catalog />} />
-                    <Route path='/reward-details' element={<CatalogDetails />} />
-                    <Route
-                        path='/vendorDashboard'
-                        element={<VendorsDashboard />}
-                    />
-                    <Route
-                        path='/vendors-transaction'
-                        element={<VendorsTransaction />}
-                    />
-                    <Route path='/store-overview' element={<StoreOverview />} />
-                    <Route path='/store-performance' element={<StorePerformance />} />
-                </Routes>            
+            <Nav
+                toggleMenu={toggleMenu}
+                menuOpen={menuOpen}
+                toggleDropdown={toggleDropdown}
+            />
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/rewards' element={<Rewards />} />
+                <Route path='/my-rewards' element={<Achievements />} />
+                <Route path='/earn-stars' element={<EarnStars />} />
+                <Route path='/catalog' element={<Catalog />} />
+                <Route path='/reward-details' element={<CatalogDetails />} />
+                <Route
+                    path='/vendorDashboard'
+                    element={<VendorsDashboard />}
+                />
+                <Route
+                    path='/vendors-transaction'
+                    element={<VendorsTransaction />}
+                />
+                <Route path='/store-overview' element={<StoreOverview />} />
+                <Route path='/store-performance' element={<StorePerformance />} />
+            </Routes>            
             <Footer />
         </>
     );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,32 +1,18 @@
-import {useState} from 'react';
+
 import Nav from '../components/nav';
 import Hero from '../components/Hero';
 import HomeSectionOne from '../components/homeSectionOne';
 import HomeSectionThree from '../components/homeSectionThree';
 
 const Home = () => {
-    const [menuOpen, setMenuOpen] = useState(false);
-
-    const toggleMenu = () => {
-        setMenuOpen(!menuOpen);
-    };
-    const toggleDropdown = () => {
-        setMenuOpen(!menuOpen);
-    };
+    
 
     return (
         <div>
-            <Nav
-                toggleMenu={toggleMenu}
-                menuOpen={menuOpen}
-                toggleDropdown={toggleDropdown}
-            />
-            <div>
-                <Hero />
-                <HomeSectionOne />
-                {/* <sectionTwo /> */}
-                <HomeSectionThree />
-            </div>
+            <Hero />
+            <HomeSectionOne />
+            {/* <sectionTwo /> */}
+            <HomeSectionThree />
         </div>
     );
 };
