453f49964dfc7771d951dff9c56034450398a268
derhuv <daphnyib@gmail.com>
2023-10-21 16:45:48 +0100

    vendor transaction data created

---
+++ src/lib/vendorsTransData.js
@@ -0,0 +1,49 @@
+import Cashicon from '../assets/icons/cashicon.png';
+
+const transactionsdata = [
+    {   id: 1,
+        payouttitle: 'Account debited',
+        image: Cashicon,
+        date: '4/09/2023',
+        amount: '$ 100.00',
+        transstatus: 'settled'
+    },
+    {   id: 2,
+        payouttitle: 'Account debited',
+        image: Cashicon,
+        date: '11/09/2023',
+        amount: '$ 300.00',
+        transstatus: 'pending'
+    },
+    {   id: 3,
+        payouttitle: 'Account debited',
+        image: Cashicon,
+        date: '18/09/2023',
+        amount: '$ 700.00',
+        transstatus: 'settled'
+    },
+    {   id: 4,
+        payouttitle: 'Account debited',
+        image: Cashicon,
+        date: '25/09/2023',
+        amount: '$ 200.00',
+        transstatus: 'pending'
+    },
+    {   id: 5,
+        payouttitle: 'Account debited',
+        image: Cashicon,
+        date: '2/10/2023',
+        amount: '$ 300.00',
+        transstatus: 'settled'
+    },
+    {   id: 6,
+        payouttitle: 'Account debited',
+        image: Cashicon,
+        date: '9/10/2023',
+        amount: '$ 100.00',
+        transstatus: 'pending'
+    }
+   
+];
+
+  export default transactionsdata;
