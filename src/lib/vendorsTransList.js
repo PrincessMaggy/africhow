ca732471ed83d5f678dcec20b62748809ad2d6b9
derhuv <daphnyib@gmail.com>
2023-10-21 11:13:46 +0100

    vendors transaction list created

---
+++ src/lib/vendorsTransList.js
@@ -0,0 +1,22 @@
+
+const transactions =[
+    {
+      id:1,
+      tabtitle:"All",
+      content: 'allMappedData'
+
+    },
+    {
+      id:2,
+      tabtitle:"Pending",
+      content:'mappedDesignData'
+        
+    },
+    {
+      id:3,
+      tabtitle:"Settled",
+      content: 'mappedData'
+        
+    }
+];
+export default transactions;
