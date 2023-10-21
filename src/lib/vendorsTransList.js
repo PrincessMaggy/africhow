99edaf1569a980c31e0e8291d50243aaa8c4f80e
derhuv <daphnyib@gmail.com>
2023-10-21 16:51:28 +0100

    vendor transaction list created

--- src/lib/vendorsTransList.js
+++ src/lib/vendorsTransList.js
@@ -1,22 +1,29 @@
+import transactionsdata from './vendorsTransData';
+
+const filteredData = transactionsdata.filter(item => item.transstatus === 'pending');
+
+const filteredDesignData = transactionsdata.filter(item => item.transstatus === 'settled');
+
+const allMappedData = transactionsdata;
 
 const transactions =[
-    {
-      id:1,
-      tabtitle:"All",
-      content: 'allMappedData'
+  {
+    id:1,
+    tabtitle:"All",
+    content: allMappedData
 
-    },
-    {
-      id:2,
-      tabtitle:"Pending",
-      content:'mappedDesignData'
-        
-    },
-    {
-      id:3,
-      tabtitle:"Settled",
-      content: 'mappedData'
-        
-    }
+  },
+  {
+    id:2,
+    tabtitle:"Pending",
+    content: filteredData 
+      
+  },
+  {
+    id:3,
+    tabtitle:"Settled",
+    content: filteredDesignData
+      
+  }
 ];
 export default transactions;
