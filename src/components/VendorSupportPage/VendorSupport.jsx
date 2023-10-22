27149d0e56a8ca3798ab73c77bca72d44134067e
Gloria Williams <willyabiodun@gmail.com>
2023-10-22 15:33:54 +0100

    changed the images to be imported

--- src/components/VendorSupportPage/VendorSupport.jsx
+++ src/components/VendorSupportPage/VendorSupport.jsx
@@ -1,5 +1,8 @@
 import { Link } from "react-router-dom"
 import Navbar from "./Navbar";
+import BackArrow from '../assets/icons/arrow_back.png';
+import NextArrow from '../assets/arrow_back_ios.png';
+
 
 
 
@@ -8,7 +11,7 @@ export default function VendorSupport() {
         <div>
             <Navbar />
             <Link to="/">
-                <img src="./src/assets/arrow_back.png" className="mx-5 mt-4 p-1 "></img>
+                <img src={BackArrow} className="mx-5 mt-4 p-1 "></img>
             </Link>
             <div className="mx-4 mt-4 font-body md:text-center">
                 <h1 className="font-bold text-3xl md:text-5xl md:mt-16 ">Help desk</h1>
@@ -19,7 +22,7 @@ export default function VendorSupport() {
                 <div>
                     <Link to="./support-form" className="flex m-auto items-center md:justify-center">
                         <p className="py-7 font-medium text-base text-secondary md:font-bold md:text-xl">Send us an email</p>
-                        <img src="./src/assets/arrow_back_ios.png" className="w-6 h-6 ml-4"/>
+                        <img src={NextArrow} className="w-6 h-6 ml-4"/>
                     </Link>
                 </div>
             </div>
