988e244182549ab82cc9f77a1cdd851c9070f15b
mutiat <mutiat900@gmail.com>
2023-10-22 23:35:36 +0100

    Added icons and images

---
+++ src/components/community/trending.jsx
@@ -0,0 +1,15 @@
+import Menu from "../assets/icons/menu.png"
+import Search from "../assets/images/search.png"
+import Addcircle from "../assets/images/add_circle.png"
+import Foodzone from "../assets/images/picture.png"
+import Heart from "../assets/images/favotite1.png"
+import Eclipse from "../assets/images/Eclipse 20.png"
+import Ofada from "../assets/images/ofada.png"
+
+const Trending= () => {
+    return (
+        <div></div>
+        )
+    }
+    
+    export default Trending
