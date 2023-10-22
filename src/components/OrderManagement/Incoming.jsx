917508b977e7497e27b243abecdd563565e99763
Taofeekat Olorunkosebi <olorunkosebitaofeekat@gmail.com>
2023-10-23 00:23:17 +0100

    working on order managemennt

--- src/components/OrderManagement/Incoming.jsx
+++ src/components/OrderManagement/Incoming.jsx
@@ -3,17 +3,17 @@ import React from 'react'
 function Incoming() {
   return (
     <div>
-        <div className='flex items-center bg-bg-order-header m-6 px-3 py-2'>
+        <div className='flex items-center bg-bg-order-header m-6 px-3 py-2 gap-4'>
             <h1 className='text-lg w-10 leading-10 bg-bg-order-nav rounded-full'>
                 HA
             </h1>
-            <div>
-                <div>
+            <div className='text-left'>
+                <p>
                     Order #24078
-                </div>
-                <div>
+                </p>
+                <p className='text-sm'>
                     Hafsat Abdul
-                </div>
+                </p>
             </div>
         </div>
     </div>
