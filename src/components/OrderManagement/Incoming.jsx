3911f1e5cd631b1f7fbef1c11221a4c142a50883
Taofeekat Olorunkosebi <olorunkosebitaofeekat@gmail.com>
2023-10-23 01:48:58 +0100

    working on order managemennt

--- src/components/OrderManagement/Incoming.jsx
+++ src/components/OrderManagement/Incoming.jsx
@@ -1,19 +1,38 @@
 import React from 'react'
+import img from './location_pin.jpg'
 
 function Incoming() {
   return (
     <div>
-        <div className='flex items-center bg-bg-order-header m-6 px-3 py-2 gap-4'>
-            <h1 className='text-lg w-10 leading-10 bg-bg-order-nav rounded-full'>
-                HA
-            </h1>
-            <div className='text-left'>
+        <div>
+            <div className='flex items-center bg-bg-order-header m-6 px-3 py-2 gap-4'>
+                <h1 className='text-lg w-10 leading-10 bg-bg-order-nav rounded-full'>
+                    HA
+                </h1>
+                <div className='text-left'>
+                    <p>
+                        Order #24078
+                    </p>
+                    <p className='text-sm'>
+                        Hafsat Abdul
+                    </p>
+                </div>
+            </div>
+
+            <div>
                 <p>
-                    Order #24078
+                    12-08-2023
                 </p>
-                <p className='text-sm'>
-                    Hafsat Abdul
+                <p>
+                •
                 </p>
+                <p>
+                    11:28 PM
+                </p>
+            </div>
+
+            <div>
+                <img src={img} alt="location-pin" />
             </div>
         </div>
     </div>
