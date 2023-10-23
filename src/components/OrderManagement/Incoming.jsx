2f5566eb7df5245ce21d7ca4e415e54d43d4fd64
Taofeekat Olorunkosebi <olorunkosebitaofeekat@gmail.com>
2023-10-23 02:47:30 +0100

    working on order management arrays

--- src/components/OrderManagement/Incoming.jsx
+++ src/components/OrderManagement/Incoming.jsx
@@ -1,42 +1,35 @@
-import React from 'react'
-import img from './location_pin.jpg'
+import React from "react";
+import location_pin from "../../assets/Images-Order-management/location_pin.jpg";
+import FoodList from "../../lib/Foodlist";
 
 function Incoming() {
   return (
     <div>
-        <div>
-            <div className='flex items-center bg-bg-order-header m-6 px-3 py-2 gap-4'>
-                <h1 className='text-lg w-10 leading-10 bg-bg-order-nav rounded-full'>
-                    HA
-                </h1>
-                <div className='text-left'>
-                    <p>
-                        Order #24078
-                    </p>
-                    <p className='text-sm'>
-                        Hafsat Abdul
-                    </p>
-                </div>
+      {FoodList.map((item, i) => (
+        <div key={i}>
+          <div className="flex items-center bg-bg-order-header m-6 px-3 py-2 gap-4">
+            <h1 className="text-lg w-10 leading-10 bg-bg-order-nav rounded-full">
+              {item.logo}
+            </h1>
+            <div className="text-left">
+              <p>Order #{item.orderCode}</p>
+              <p className="text-sm">{item.name}</p>
             </div>
+          </div>
 
-            <div>
-                <p>
-                    12-08-2023
-                </p>
-                <p>
-                •
-                </p>
-                <p>
-                    11:28 PM
-                </p>
-            </div>
+          <div>
+            <p>{item.date}</p>
+            <p>•</p>
+            <p>{item.time}</p>
+          </div>
 
-            <div>
-                <img src={img} alt="location-pin" />
-            </div>
+          <div>
+            <img src={location_pin} alt="location-pin" />
+          </div>
         </div>
+      ))}
     </div>
-  )
+  );
 }
 
-export default Incoming
+export default Incoming;
---
+++ src/lib/Foodlist.js
@@ -0,0 +1,100 @@
+import Amala from '../assets/Images-Order-management/Amala&Ewedu.jpg'; 
+import BeansPlantain from '../assets/Images-Order-management/Beans&Plantain.jpg';
+import Egusi from '../assets/Images-Order-management/Egusi&Poundo.jpg'; 
+import EwaAgoyin from '../assets/Images-Order-management/EwaAgoyin.jpg'; 
+import FriedRice from '../assets/Images-Order-management/Friedrice.jpg'; 
+import GhanaJollof from '../assets/Images-Order-management/GhanaJollof.jpg'; 
+import Oha from '../assets/Images-Order-management/Oha.jpg'; 
+import Waakye from '../assets/Images-Order-management/Waakye.jpg'; 
+
+
+const FoodList = [
+    {
+        id: 1,
+        logo: "HA",
+        name: "Hafsat Abdul",
+        orderCode: 24071,
+        date: "12-08-2023",
+        time: "11:28 PM",
+        address: "49 Featherstone Street, EC1Y 8SY, London",
+        Foodname1: "Amala & Ewedu",
+        Desc1: "Amala, Ewedu and Gbegiri",
+        img1: Amala,
+        Price1: 20,
+        Qty1: 2,
+        Foodname2: "Fried Rice & Chicken",
+        Desc2: "Fried Rice, Salad and Chicken",
+        img2: FriedRice,
+        Price2: 18,
+        Qty2: 2,
+        // Total: 38,
+    },
+
+
+    {
+        id: 2,
+        logo: "HA",
+        name: "Hafsat Abdul",
+        orderCode: 24071,
+        date: "12-08-2023",
+        time: "11:28 PM",
+        address: "49 Featherstone Street, EC1Y 8SY, London",
+        Foodname1: "Amala & Ewedu",
+        Desc1: "Amala, Ewedu and Gbegiri",
+        img1: Amala,
+        Price1: 20,
+        Qty1: 2,
+        Foodname2: "Fried Rice & Chicken",
+        Desc2: "Fried Rice, Salad and Chicken",
+        img2: FriedRice,
+        Price2: 18,
+        Qty2: 2,
+        // Total: 38,
+    },
+
+
+    {
+        id: 3,
+        logo: "HA",
+        name: "Hafsat Abdul",
+        orderCode: 24071,
+        date: "12-08-2023",
+        time: "11:28 PM",
+        address: "49 Featherstone Street, EC1Y 8SY, London",
+        Foodname1: "Amala & Ewedu",
+        Desc1: "Amala, Ewedu and Gbegiri",
+        img1: Amala,
+        Price1: 20,
+        Qty1: 2,
+        Foodname2: "Fried Rice & Chicken",
+        Desc2: "Fried Rice, Salad and Chicken",
+        img2: FriedRice,
+        Price2: 18,
+        Qty2: 2,
+        // Total: 38,
+    },
+
+
+    {
+        id: 4,
+        logo: "HA",
+        name: "Hafsat Abdul",
+        orderCode: 24071,
+        date: "12-08-2023",
+        time: "11:28 PM",
+        address: "49 Featherstone Street, EC1Y 8SY, London",
+        Foodname1: "Amala & Ewedu",
+        Desc1: "Amala, Ewedu and Gbegiri",
+        img1: Amala,
+        Price1: 20,
+        Qty1: 2,
+        Foodname2: "Fried Rice & Chicken",
+        Desc2: "Fried Rice, Salad and Chicken",
+        img2: FriedRice,
+        Price2: 18,
+        Qty2: 2,
+        // Total: 38,
+    }
+]
+
+export default FoodList;
