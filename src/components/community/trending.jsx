34348434c790416f23d756cf54e7f393f357ebe8
mutiat <mutiat900@gmail.com>
2023-10-24 14:36:28 +0100

    Added images

--- src/components/community/trending.jsx
+++ src/components/community/trending.jsx
@@ -9,10 +9,11 @@ import Ofada from "../../assets/images/ofada.png"
 
 const TrendingPage= () => {
     return (
-<div className="py-20 h-screen bg-gray-300 px-2">
-  <div className="max-w-md mx-auto rounded-lg overflow-hidden md:max-w-xl">
+      
+<div className="py-15 max-h-screen  bg-gray-300 m-2 px-2">
+  <div className="max-w-md mx-auto rounded-lg overflow-hidden md:max-w-xl max-w">
   <div className="md:flex">
-      <div className="w-full p-3 sm:flex sm:items-center">
+      <div className="w-full p-3 sm:flex sm:items-center ">
         <div className="mr-3">
         <div className="relative ">
         <img src={Search} className='w-[24px] h-[24px] absolute text-gray-400 top-5 left-4'/>
@@ -23,7 +24,7 @@ const TrendingPage= () => {
         
         <button className=" inline-flex items-center pt-4 pb-4 bg-[#33CC9F] text-gray-800 text-sm font-medium rounded">
         <img src={Addcircle} alt="Addcircle image" className=' relative w-[24px]  mr-4 left-2'/>
-        <span class="mr-2">Add point</span>
+        <span className="mr-2">Add point</span>
         </button>
 
 
@@ -32,7 +33,36 @@ const TrendingPage= () => {
    
   </div>
 </div>
+        <div >
+        <div className=" w-full p-3 sm:flex flex-row justify-between items-center px-4">
+        
+        <div>
+          <p className="font-bold text-lg">Trending</p>
+         </div>
+         <div>
+          <p className="font-semibold text-lg">See All </p>
+         </div>
+        </div>
+       </div>
+       {/* IMAGES AFTER SEARCH BAR*/}
+<div className=" md:flex flex-row justify-between items-center px-4 gap-4 max-h-96">
+<div className="mx-auto flex w-96 flex-col justify-center bg-white rounded-2xl shadow-xl ">
+  <img  src={Foodzone} className="w-96 rounded-t-2xl object-cover object-center"/>
+  <div className="p-4">
+  <h1 className="text-lg font-bold pt-2"> African Food Recipe </h1>
+  <p className="text-xs pt-2 text-[#33CC9F]"> See more</p>
+  </div>
   
+  </div>
+  <div className="mx-auto flex w-96 flex-col justify-center bg-white rounded-2xl shadow-xl ">
+  <img  src={Foodzone} className="w-96 rounded-t-2xl object-cover object-center"/>
+  <div className="p-4">
+  <h1 className="text-lg font-bold pt-2"> African Food Recipe </h1>
+  <p className="text-xs pt-2 text-[#33CC9F]"> See more</p>
+  </div>
+  
+  </div>
+      </div>
 </div>
        
         )
