7ef6c01f52734be012f87c1bac73f7d0eb121eee
Maria-goretti Jiwuaku <mariagorettiduruigbo@gmail.com>
2023-10-24 00:54:18 -0700

    changes

--- src/pages/ReviewsCard.jsx
+++ src/pages/ReviewsCard.jsx
@@ -2,49 +2,51 @@ import React from 'react';
 
 const ReviewsCard = () => {
   return (
-    <div className="w-96 h-96 relative bg-white">
-      {/* Header */}
-      <div className="w-96 h-11 left-0 top-0 absolute">
-        {/* Header content */}
-      </div>
-
+    <div className="max-w-3xl mx-auto w-full relative bg-white p-8">
       {/* User Information */}
-      <div className="left-[16px] top-[143px] absolute flex-col justify-start items-start gap-4 inline-flex">
+      <div className="flex-col justify-start items-start gap-4">
         {/* User details */}
         <div className="flex-col justify-start items-start gap-3 flex">
           {/* User profile */}
           <div className="flex-col justify-end items-start gap-2 flex">
             {/* Profile image and user details */}
+            <div className="flex items-center">
+              <img className="w-12 h-12 rounded-full" src="https://via.placeholder.com/50x50" alt="user" />
+              <div className="flex-col justify-start items-start gap-1.5 ml-4">
+                <div className="text-center text-zinc-950 text-xl font-medium font-inter tracking-tight">Solomon Enakhe</div>
+                <div className="flex items-center gap-1">
+                  <div className="text-black text-xs font-medium font-manrope tracking-tight">(Rating)</div>
+                  <div className="w-2 h-2 bg-zinc-300 rounded-full" />
+                  <div className="text-black text-xs font-medium font-manrope tracking-tight">12/10/2023</div>
+                </div>
+              </div>
+            </div>
           </div>
 
           {/* Review content */}
-          <div className="w-96">
-            <span className="text-neutral-600 text-sm font-normal font-['Manrope'] leading-tight tracking-tight">
-              {/* Review text */}
+          <div className="w-full">
+            <span className="text-neutral-600 text-sm font-normal font-manrope leading-tight tracking-tight">
+              Enjoyed a delightful meal at this restaurant. The ambiance was great, and the service was excellent. I highly recommend it!
             </span>
-            <span className="text-emerald-400 text-sm font-semibold font-['Manrope'] leading-tight tracking-tight">See more</span>
+            <span className="text-emerald-400 text-sm font-semibold font-manrope leading-tight tracking-tight">See more</span>
           </div>
 
           {/* Message input */}
-          <div className="self-stretch h-28 p-4 bg-white border border-zinc-100 justify-start items-start gap-2.5 inline-flex">
-            <div className="text-center text-slate-500 text-sm font-medium font-['Manrope'] tracking-tight">Type your message</div>
+          <div className="self-stretch h-20 p-4 bg-white border border-zinc-100 mt-4">
+            <div className="text-slate-500 text-sm font-medium font-manrope tracking-tight">Type your message</div>
           </div>
         </div>
 
         {/* Send button */}
-        <div className="px-4 py-1 bg-emerald-400 justify-center items-center gap-2.5 inline-flex">
-          <div className="text-center text-white text-base font-medium font-['Manrope'] tracking-tight">Send</div>
+        <div className="px-4 py-2 bg-emerald-400 mt-4">
+          <div className="text-center text-white text-base font-medium font-manrope tracking-tight cursor-pointer">Send</div>
         </div>
       </div>
 
       {/* Review and Ratings Header */}
-      <div className="w-96 h-12 left-0 top-[44px] absolute bg-white border-b border-zinc-300">
-        <div className="left-[16px] top-[12px] absolute justify-start items-center gap-2 inline-flex">
-          {/* Review icon and text */}
-          <div className="w-6 h-6 relative bg-zinc-950">
-            {/* Lines in the icon */}
-          </div>
-          <div className="text-zinc-950 text-lg font-medium font-['Manrope'] leading-relaxed">Reviews and Ratings</div>
+      <div className="mt-8 border-b border-zinc-300">
+        <div className="flex items-center gap-2">
+          {/* <div className="text-zinc-950 text-lg font-medium font-manrope leading-relaxed">Reviews and Ratings</div> */}
         </div>
       </div>
     </div>
