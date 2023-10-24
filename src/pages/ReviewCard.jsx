4af68ecb00a7a62b8ec9563f5fffaca964096c0c
Maria-goretti Jiwuaku <mariagorettiduruigbo@gmail.com>
2023-10-24 10:00:54 -0700

    created the review page and components

---
+++ src/pages/ReviewCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReviewNav from '../components/Reviews/ReviewNav'
+
+const ReviewCard = () => {
+  return (
+    <div>
+        <ReviewNav/>
+      Reviews will be here
+    </div>
+  )
+}
+
+export default ReviewCard
