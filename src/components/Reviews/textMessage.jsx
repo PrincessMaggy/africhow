5e71a6651a1978fe48259053613a9454872f3e42
Maria-goretti Jiwuaku <mariagorettiduruigbo@gmail.com>
2023-10-24 11:23:59 -0700

    made changes in the design

--- src/components/Reviews/textMessage.jsx
+++ src/components/Reviews/textMessage.jsx
@@ -7,16 +7,17 @@ const TextMessage = ({
     message,
   }) => {
     return (
-      <TailwindCSS>
+      
         <div className="flex flex-col gap-4">
           <div className="flex items-center justify-between">
             <div className="font-bold">{sender}</div>
-            <div className="text-gray-500">{date}</div>
             <div className="text-yellow-500">{rating}</div>
+            <div className="text-gray-500">{date}</div>
+
           </div>
           <div className="text-gray-700">{message}</div>
         </div>
-      </TailwindCSS>
+      
     );
   };
   
