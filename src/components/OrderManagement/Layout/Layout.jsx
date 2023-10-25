905a298d5ce2cb1060484ea2a86d73f9d46aca8a
Taofeekat Olorunkosebi <olorunkosebitaofeekat@gmail.com>
2023-10-25 04:28:44 +0100

    working on the history section

--- src/components/OrderManagement/Layout/Layout.jsx
+++ src/components/OrderManagement/Layout/Layout.jsx
@@ -1,15 +1,18 @@
+import { useState } from "react";
 import { Outlet, Link } from "react-router-dom";
 
 const Layout = () => {
+    const [headerBg, setHeaderBg] = useState()
+
     return (
         <>
             <div>
                 Order Management
             </div>
             <nav>
-                <ul className="flex justify-between px-6 py-3 my-5 text-sm text-gray-500 bg-bg-order-nav">
-                    <li className="flex items-center justify-center gap-1 ">
-                        <Link to='/incoming'>Incoming</Link> <div className="w-6 text-xs leading-6 text-center rounded-full bg-bg-order-notify text-slate-100">8</div>
+                <ul className="flex justify-between px-6 py-2 my-5 text-sm text-gray-500 bg-bg-order-nav items-center">
+                    <li className="flex items-center justify-center gap-1">
+                        <Link to='/incoming' className="py-2">Incoming</Link> <div className="w-6 text-xs leading-6 text-center rounded-full bg-bg-order-notify text-slate-100"></div>
                     </li>
                     <li className="flex items-center justify-center gap-1 ">
                         <Link to='/outgoing'>Outgoing</Link> <div className="w-6 text-xs leading-6 text-center rounded-full bg-bg-order-notify text-slate-100"></div>
