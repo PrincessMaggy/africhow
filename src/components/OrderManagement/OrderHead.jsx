e620219191227161ead7f7bf58890370fde5283e
Taofeekat Olorunkosebi <olorunkosebitaofeekat@gmail.com>
2023-10-25 15:12:41 +0100

    working on the dropdown of the active headers

--- src/components/OrderManagement/OrderHead.jsx
+++ src/components/OrderManagement/OrderHead.jsx
@@ -5,17 +5,19 @@ import React from 'react';
 const OrderNav = () => {
     const location = useLocation();
 
+    {location.pathname === '/rewards' ? 'active-link' : 'font-normal text-base text-#808080'}
+
   return (
     <nav>
         <ul className="flex items-center justify-between px-6 py-2 my-5 text-sm text-gray-500 bg-bg-order-nav">
-            <li className="flex items-center justify-center gap-1">
-                <Link to='/incoming' className="py-2">Incoming</Link> <div className="w-6 text-xs leading-6 text-center rounded-full bg-bg-order-notify text-slate-100"></div>
+                <li className={location.pathname === '/incoming' ? 'text-white bg-bg-order-active flex items-center justify-center gap-1 px-2' : ' text-gray-500 bg-bg-order-nav flex items-center justify-center gap-1 px-2'}>
+                <Link to='/incoming' className="py-2">Incoming</Link> <div className="w-6 text-xs leading-6 text-center rounded-full bg-bg-order-notify text-slate-100">8</div>
             </li>
-            <li className="flex items-center justify-center gap-1 ">
-                <Link to='/outgoing'>Outgoing</Link> <div className="w-6 text-xs leading-6 text-center rounded-full bg-bg-order-notify text-slate-100"></div>
+            <li className={location.pathname === '/outgoing' ? 'text-white bg-bg-order-active flex items-center justify-center gap-1 px-2' : ' text-gray-500 bg-bg-order-nav flex items-center justify-center gap-1 px-2'}>
+                <Link to='/outgoing' className='py-2'>Outgoing</Link> <div className="w-6 text-xs leading-6 text-center rounded-full bg-bg-order-notify text-slate-100">8</div>
             </li>
-            <li>
-                <Link to='/history'>History</Link>
+            <li className={location.pathname === '/history' ? 'text-white bg-bg-order-active flex items-center justify-center gap-1 px-6' : ' text-gray-500 bg-bg-order-nav flex items-center justify-center gap-1 px-2'}>
+                <Link to='/history' className='py-2'>History</Link>
             </li>
         </ul>
     </nav>
