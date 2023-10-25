e99718a6aa2df864ec9d4856cf63af5b91d20338
Maggy <edoziemagdalene@gmail.com>
2023-10-25 10:32:18 +0100

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -46,19 +46,14 @@ import Layout from './components/OrderManagement/Layout/Layout';
 import NewMeal from './components/addMealItem';
 import Savechangeprofile from './components/Savechangeprofile';
 
-
 function App() {
     return (
         <>
             <Routes>
                 <Route path='/' element={<Home />} />
-                {/* <Route path='/community' element={<Community />} /> */}
-                <Route path='/signup' element={<Signup/>} />
-                <Route path='/login' element={<Login/>} />
+                <Route path='/signup' element={<Signup />} />
+                <Route path='/login' element={<Login />} />
                 <Route path='/contact-us' element={<Support />} />
-                {/* <Route path='/community' element={<Community />} /> */}
-                {/* <Route path='/signup' element={<Signup />} /> */}
-                {/* <Route path='/login' element={<Login />} /> */}
 
                 <Route path='/contact-us' element={<Support />} />
 
