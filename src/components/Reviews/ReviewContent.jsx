7284a3692c86320c7f5ab115a246b1b17f5af87d
Maria-goretti Jiwuaku <mariagorettiduruigbo@gmail.com>
2023-10-25 01:24:25 -0700

    for the review contents

--- src/components/Reviews/ReviewContent.jsx
+++ src/components/Reviews/ReviewContent.jsx
@@ -1,25 +1,56 @@
 import React from 'react'
 
-const TextMessage = ({
-    sender,
-    rating,
-    date,
-    message,
-  }) => {
-    return (
-      
-        <div className="flex flex-col gap-4">
-          <div className="flex items-center justify-between">
-            <div className="font-bold">{sender}</div>
-            <div className="text-yellow-500">{rating}</div>
-            <div className="text-gray-500">{date}</div>
-
+const ReviewContent = () => {
+  return (
+    <div>
+      {/* User Information */}
+      <div className="left-[0] top-[150px]  flex-col justify-start items-start gap-4 inline-flex">
+        {/* User details */}
+        <div className="flex-col justify-start items-start gap-3 flex">
+          {/* User profile */}
+          <div className="flex-col justify-end items-start gap-2 flex">
+            {/* Profile image and user details */}
+            <div className="flex items-center">
+              <img className="w-9 h-9 rounded-full" src={Solomon} alt="user" />
+              <div className="flex-col justify-start items-start gap-1.5 inline-flex">
+                <div className="text-center text-zinc-950 text-base font-medium font-['Inter'] tracking-tight">Solomon Enahke</div>
+                <div className="justify-center items-center gap-1 inline-flex">
+                  <div className="justify-start items-start flex" />
+                  <div className="text-center text-black text-xs font-medium font-['Manrope'] tracking-tight">
+                    <img src={Star} alt="star" />
+                  </div>
+                  <div className="text-center text-black text-xs font-medium font-['Manrope'] tracking-tight">
+                    <img src={Star} alt="star" />
+                  </div>
+                  <div className="text-center text-black text-xs font-medium font-['Manrope'] tracking-tight">
+                    <img src={Star} alt="star" />
+                  </div>
+                  <div className="text-center text-black text-xs font-medium font-['Manrope'] tracking-tight">
+                    <img src={Star} alt="star" />
+                  </div>
+                  <div className="text-center text-black text-xs font-medium font-['Manrope'] tracking-tight">
+                    <img src={Star} alt="star" />
+                  </div><div className="text-center text-black text-xs font-medium font-['Manrope'] tracking-tight">
+                    Rating
+                  </div>
+                  
+                  <div className="text-center text-black text-xs font-medium font-['Manrope'] tracking-tight">12/10/2023</div>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="text-gray-700">{message}</div>
+
+          {/* Review content */}
+          <div className="w-96">
+            <span className="text-neutral-600 text-sm font-normal font-['Manrope'] leading-tight tracking-tight">
+              Enjoyed a delightful meal at this restaurant. The ambiance was great, and the service was excellent. I highly recommend it!
+            </span>
+            <span className="text-emerald-400 text-sm font-semibold font-['Manrope'] leading-tight tracking-tight">See more</span>
+          </div>
         </div>
-      
-    );
-  };
-  
-  export default TextMessage;
-  
+      </div>
+    </div>
+  )
+}
+
+export default ReviewContent
