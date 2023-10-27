739f5f1c7e47be19d0fd6c0781498c29c9d0ac88
Maggy <edoziemagdalene@gmail.com>
2023-10-27 08:46:13 +0100

    fixed all conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -9,42 +9,26 @@ import Support from './pages/Support';
 import Rewards from './pages/Rewards';
 import Signup from './pages/Signup';
 import OnBoardingSignUpForm from './pages/OnBoardingSignUpForm';
-import Cancelprofilecard from './pages/Cancelprofilecard';
-import Profilesavedsuccess from './pages/Profilesavedsuccess';
-import Profileerrormessage from './pages/Profileerrormessage';
 import FormSuccess from './pages/FormSuccess';
 import ConfirmPassword from './pages/ConfirmPassword';
 import ForgotPassword from './pages/ForgotPassword';
 import ReviewCard from './pages/ReviewCard';
 import ReviewReply from './pages/ReviewReply';
 import Vendorprofile from './pages/Vendorprofile';
-import Vendorworkhour from './pages/Vendorworkhour';
 import Achievements from './pages/Achievements';
 import Catalog from './pages/Catalog';
 import CatalogDetails from './pages/CatalogDetails';
 import EarnStars from './pages/EarnStars';
-import SuccessRewards from './pages/SuccessRewards';
 import VendorDashboard from './pages/VendorDashboard';
 import VendorTransactions from './pages/VendorTransactions';
 import VendorPayout from './pages/VendorPayout';
- import Listings from './pages/MealListing';
+import Listings from './pages/MealListing';
 import Login from './pages/Login';
 import SuccessRewards from './pages/SuccessRewards';
-import ReviewCard from './pages/ReviewCard';
-import ReviewReply from './pages/ReviewReply';
- import NewMeal from './pages/AddNewMeal';
-import Vendorprofile from "./pages/Vendorprofile";
-import Vendorprofilechange from "./pages/Vendorprofilechange";
-
-
+import NewMeal from './pages/AddNewMeal';
+import Vendorprofilechange from './pages/Vendorprofilechange';
 
 // importing components
-import Achievements from './components/Rewards/Achievements';
-import Catalog from './components/Rewards/catalog/Catalog';
-import EarnStars from './components/Rewards/EarnStars';
-import CatalogDetails from './components/Rewards/catalog/CatalogDetails';
-import VendorsDashboard from './components/vendorsPayment/vendorsDashboard';
-import VendorsTransaction from './components/vendorsPayment/vendorsTransaction';
 import StoreOverview from './components/storePerformance/StoreOverview';
 import StorePerformance from './components/storePerformance/StorePerformance';
 import VendorSupport from './components/VendorSupportPage/VendorSupport';
@@ -55,33 +39,24 @@ import TrendingComponent from './components/community/trending';
 import Post from './components/community/post';
 import Blog from './components/community/blog';
 import Recipes from './components/community/recipes';
-import VendorsPayoutMethod from './components/vendorsPayment/VendorsPayoutMethod';
- import Outgoing from './components/OrderManagement/Outgoing';
- import Incoming from './components/OrderManagement/Incoming';
-import Cancelprofilecard from "./components/Cancelprofilecard";
-import Vendorworkhour from "./components/Vendorworkhour";
-import Outgoing from './components/OrderManagement/Outgoing';
-import Incoming from './components/OrderManagement/Incoming';
 import Cancelprofilecard from './components/Cancelprofilecard';
 import Vendorworkhour from './components/Vendorworkhour';
+import Outgoing from './components/OrderManagement/Outgoing';
+import Incoming from './components/OrderManagement/Incoming';
 import History from './components/OrderManagement/History';
 import TestWork from './components/auth/testProfile';
 import NotificationPage from './components/Notification/notification';
-import NotificationSuccess from './components/Notification/notificationSuccesful'
-
+import NotificationSuccess from './components/Notification/notificationSuccesful';
 
 function App() {
     return (
         <>
             <Routes>
-        
- 
-                <Route path ="/trending" element = {<TrendingComponent/>}/>
-                <Route path= "/post" element = {<Post/>}/>
-                <Route path= "/blog" element = {<Blog/>}/>
-                <Route path= "/recipes" element = {<Recipes/>}/>
-    
-                          
+                <Route path='/trending' element={<TrendingComponent />} />
+                <Route path='/post' element={<Post />} />
+                <Route path='/blog' element={<Blog />} />
+                <Route path='/recipes' element={<Recipes />} />
+
                 <Route path='/' element={<Home />} />
                 <Route path='/signup' element={<Signup />} />
                 <Route path='/login' element={<Login />} />
@@ -96,8 +71,8 @@ function App() {
                     path='/rewards/reward-details/:itemId'
                     element={<CatalogDetails />}
                 />
-                
-                <Route path='/store-overview' element={< StoreOverview />} />
+
+                <Route path='/store-overview' element={<StoreOverview />} />
                 <Route path='/success/:id' element={<SuccessRewards />} />
                 <Route
                     path='/vendors-dashboard'
@@ -182,7 +157,6 @@ function App() {
                 <Route path='/review card' element={<ReviewCard />} />
                 <Route path='/trending' element={<TrendingComponent />} />
                 <Route path='/post' element={<Post />} />
-                <Route path='/review card' element={<ReviewCard />} />
                 <Route path='/review-reply' element={<ReviewReply />} />
                 <Route path='/hours' element={<TestWork />} />
                 <Route path='/notification' element={<NotificationPage />} />
