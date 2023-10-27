08114ed0016fbdc14e81a36b89cec531c0a1c268
Felicity Essien <felicityabel2016@gmail.com>
2023-10-27 05:38:31 +0100

    App.jsx conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -10,17 +10,17 @@ import Support from './pages/Support';
 import Rewards from './pages/Rewards';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
-import OnBoardingSignUpForm from './pages/OnBoardingSignUpForm';
+// import OnBoardingSignUpForm from './pages/OnBoardingSignUpForm';
 // import Cancelprofilecard from './pages/Cancelprofilecard';
-import Profilesavedsuccess from './pages/Profilesavedsuccess';
-import Profileerrormessage from './pages/Profileerrormessage';
+// import Profilesavedsuccess from './pages/Profilesavedsuccess';
+// import Profileerrormessage from './pages/Profileerrormessage';
 import FormSuccess from './pages/FormSuccess';
 import ConfirmPassword from './pages/ConfirmPassword';
 import ForgotPassword from './pages/ForgotPassword';
 import ReviewCard from './pages/ReviewCard';
 import ReviewReply from './pages/ReviewReply';
 import Vendorprofile from './pages/Vendorprofile';
-import Vendorworkhour from './pages/Vendorworkhour';
+// import Vendorworkhour from './pages/Vendorworkhour';
 import Achievements from './pages/Achievements';
 import Catalog from './pages/Catalog';
 import CatalogDetails from './pages/CatalogDetails';
@@ -51,7 +51,7 @@ import Outgoing from './components/OrderManagement/Outgoing';
 import Incoming from './components/OrderManagement/Incoming';
 import History from './components/OrderManagement/History';
 import NewMeal from './pages/AddNewMeal';
-import Savechangeprofile from './components/Savechangeprofile';
+// import Savechangeprofile from './components/Savechangeprofile';
 import NotificationPage from './components/Notification/notification';
 import NotificationSuccess from './components/Notification/notificationSuccesful'
 
@@ -102,10 +102,10 @@ function App() {
                 {/* <Route path='/passwordsuccess' element={<Pass} /> */}
                 <Route path='/meallisting/:userId' element={<Listings />} />
                 <Route path='/add-new-meal' element={<NewMeal />} />
-                <Route
+                {/* <Route
                     path='/account setup'
                     element={<OnBoardingSignUpForm />}
-                />
+                /> */}
                 <Route
                     path='/login successful'
                     element={
@@ -146,14 +146,14 @@ function App() {
                 <Route path='/history' element={<History />} />
                 <Route path='/vendorprofile' element={<Vendorprofile />} />
                 <Route path='/vendorworkhour' element={<Vendorworkhour />} />
-                <Route
+                {/* <Route
                     path='/vendorprofilechange'
                     element={<Vendorprofilechange />}
-                />
-                <Route
+                /> */}
+                {/* <Route
                     path='/cancelprofilecard'
                     element={<Cancelprofilecard />}
-                />
+                /> */}
                 <Route path='/trending' element={<TrendingComponent />} />
                 <Route path='/post' element={<Post />} />
                 <Route path='/review card' element={<ReviewCard />} />
