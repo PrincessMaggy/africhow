d04538c2be53a2a901c36b926ffd8b8848efb867
Maggy <edoziemagdalene@gmail.com>
2023-10-30 20:35:57 +0100

    fixed nav

--- src/components/NewNav/NewNavBar.jsx
+++ src/components/NewNav/NewNavBar.jsx
@@ -1,71 +1,74 @@
 // Navbar.js
-import { useState } from 'react';
-import { Link } from 'react-router-dom'; // If you are using React Router
+import {useState} from 'react';
+import {Link} from 'react-router-dom'; // If you are using React Router
 import PropTypes from 'prop-types';
 import navList from '../../lib/NewNavList';
-import './newnav.css'
-import hamburger from '../../assets/NavIcons/hamburger.svg'
-import menuClose from '../../assets/NavIcons/closeIcon.svg'
+import './newnav.css';
+import hamburger from '../../assets/NavIcons/hamburger.svg';
+import menuClose from '../../assets/NavIcons/closeIcon.svg';
 import ChevronRight from '../../assets/NavIcons/ChevronRight';
-import { useLocation } from 'react-router-dom';
-import React from 'react';
+import {useLocation} from 'react-router-dom';
 
 const NewNavbar = () => {
-  const location = useLocation();
-  const currentPathname = location.pathname;
-  const [openNav, setOpenNav] = useState(false)
-  return (
-    <div className="navbar relative ">
-      <div className="harmburge mx-4 py-6 flex">
-        <img
-          src={hamburger}
-          alt='menu open'
-          onClick={() => setOpenNav(true)}
-        />
-        <span className='pl-2 text-lg font-medium'>Menu</span>
-      </div>
-      {openNav && (
-        <div className="navlists w-full absolute top-0 bottom-0 h-screen bg-white z-10">
-          <div className="harmburge mx-4 py-6 flex">
-            <img
-              src={menuClose}
-              alt=''
-              srcSet=''
-              onClick={() => setOpenNav(false)}
-            />
-            <span className='pl-2 text-lg font-medium'>Menu</span>
-          </div>
-          {navList.map((item, index) => (
-            <Link to={item.route} key={index}>
-             <div className="navitem mx-4 py-3" >
-            <div className="flex justify-between">
-              <div className="icon flex">
-              {item.icon}
-                <span 
-                  className={`text-large font-medium pl-1 ${currentPathname === item.route ? 'activeLink' : ''}`}
-
-                >{item.title}</span>
-              </div>
-              <div className="arrow">
-                <ChevronRight isActive={currentPathname === item.route}/>
-              </div>
+    const location = useLocation();
+    const currentPathname = location.pathname;
+    const [openNav, setOpenNav] = useState(false);
+    return (
+        <div className='navbar relative '>
+            <div className='harmburge mx-4 py-6 flex'>
+                <img
+                    src={hamburger}
+                    alt='menu open'
+                    onClick={() => setOpenNav(true)}
+                />
+                <span className='pl-2 text-lg font-medium'>Menu</span>
             </div>
-          </div>
-            </Link>
-           
-          ))}
-          
+            {openNav && (
+                <div className='navlists w-full absolute top-0 bottom-0 h-screen bg-white z-10'>
+                    <div className='harmburge mx-4 py-6 flex'>
+                        <img
+                            src={menuClose}
+                            alt=''
+                            srcSet=''
+                            onClick={() => setOpenNav(false)}
+                        />
+                        <span className='pl-2 text-lg font-medium'>Menu</span>
+                    </div>
+                    {navList.map((item, index) => (
+                        <Link to={item.route} key={index}>
+                            <div className='navitem mx-4 py-3'>
+                                <div className='flex justify-between'>
+                                    <div className='icon flex'>
+                                        {item.icon}
+                                        <span
+                                            className={`text-small font-medium pl-1 ${
+                                                currentPathname === item.route
+                                                    ? 'activeLink'
+                                                    : ''
+                                            }`}
+                                        >
+                                            {item.title}
+                                        </span>
+                                    </div>
+                                    <div className='arrow'>
+                                        <ChevronRight
+                                            isActive={
+                                                currentPathname === item.route
+                                            }
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                        </Link>
+                    ))}
+                </div>
+            )}
         </div>
-      )}
-
-
-     
-    </div>
-  );
+    );
 };
 
 NewNavbar.propTypes = {
-  navTitle: PropTypes.string,
+    navTitle: PropTypes.string,
 };
 
 export default NewNavbar;
--- src/lib/NewNavList.jsx
+++ src/lib/NewNavList.jsx
@@ -1,59 +1,47 @@
-import RewardIcon from '../assets/NavIcons/RewardIcon.jsx';
-import Contract from '../assets/NavIcons/Contract.jsx';
+// import RewardIcon from '../assets/NavIcons/RewardIcon.jsx';
+// import Contract from '../assets/NavIcons/Contract.jsx';
 
 const navList = [
-  
-  {
-    title: "Loyalty Rewards",
-    route: "/rewards",
-    icon: <RewardIcon />,
-  },
+    {
+        title: 'Loyalty Rewards',
+        route: '/rewards',
+        // icon: <RewardIcon />,
+    },
 
-  {
-    title: "Store Performance",
-    route: "/store-overview",
-  },
+    {
+        title: 'Store Performance',
+        route: '/store-overview',
+    },
 
-  {
-    title: "Help",
-    route: "/vendorsupport",
-  },
+    {
+        title: 'Help',
+        route: '/vendorsupport',
+    },
+    {
+        title: 'Reviews & Ratings',
+        route: '/review card',
+    },
+    {
+        title: 'Payout',
+        route: '/vendors-dashboard',
+    },
 
-  {
-    title: "Help",
-    route: "/supportform",
-  },
+    {
+        title: 'Transaction',
+        route: '/vendors-transaction',
+    },
 
-  {
-    title: "Help",
-    route: "/supportsuccess",
-  },
-{
-  title: "Reviews and Ratings",
-  route: "/review card",
-},
-  {
-    
-    title: "Payout",
-    route: "/vendors-dashboard",
-  },
+    {
+        title: 'Payout method',
+        route: '/vendors-payout-method',
+    },
 
-  {
-    title: "Transaction",
-    route: "/vendors-transaction",
-  },
-
-  {
-    title: "Payout method",
-    route: "/vendors-payout-method",
-  },
-
-  {
-    title: "Order Management",
-    route: "/incoming",
-    icon: <Contract />,
-  },
-  // Add more navigation items as needed
+    {
+        title: 'Order Mgt.',
+        route: '/incoming',
+        // icon: <Contract />,
+    },
+    // Add more navigation items as needed
 ];
 
 export default navList;
