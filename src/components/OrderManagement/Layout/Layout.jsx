62d6fb9997da6a47558b5020e275fde5c8e0f45f
mutiat <mutiat900@gmail.com>
2024-02-07 20:13:10 +0100

    Made Layout.js responsive

--- src/components/OrderManagement/Layout/Layout.jsx
+++ src/components/OrderManagement/Layout/Layout.jsx
@@ -10,29 +10,29 @@ const Layout = () => {
                 Order Management
             </div>
             <nav>
-                <ul className="lg:flex justify-between md:block px-6 py-2 my-5 text-sm text-gray-500 bg-bg-order-nav items-center">
+                <ul className="lg:flex justify-between px-6 py-2 my-5 text-sm text-gray-500 bg-bg-order-nav items-center md:block">
                     <li className="flex items-center justify-center gap-1">
                         <Link to='/incoming' className="py-2">Incoming</Link> <div className="w-6 text-xs leading-6 text-center rounded-full bg-bg-order-notify text-slate-100"></div>
                     </li>
                     <li className="flex items-center justify-center gap-1 ">
-                        <Link to='/outgoing'>Outgoing</Link> <div className="w-6 text-xs leading-6 text-center rounded-full bg-bg-order-notify text-slate-100"></div>
+                        <Link to='/outgoing' className="py-2">Outgoing</Link> <div className="w-6 text-xs leading-6 text-center rounded-full bg-bg-order-notify text-slate-100"></div>
                     </li>
-                    <li>
-                        <Link to='/history'>History</Link>
+                    <li className="flex items-center justify-center gap-1 ">
+                        <Link to='/history' className="py-2">History</Link>  <div className="w-6 text-xs leading-6 text-center rounded-full bg-bg-order-notify text-slate-100"></div>
                     </li>
 
                 
-                    <li>
-                        <Link to='/pending'>Delivery Enroute</Link>
+                    <li className="flex items-center justify-center gap-1">
+                        <Link to='/pending className="py-2"'>Delivery Enroute</Link>  <div className="w-6 text-xs leading-6 text-center rounded-full bg-bg-order-notify text-slate-100"></div>
                     </li>
-                    <li>
-                        <Link to='/complete'>Delivery Completed</Link>
+                    <li className="flex items-center justify-center gap-1">
+                        <Link to='/complete' className="py-2">Delivery Completed</Link>  <div className="w-6 text-xs  leading-6 text-center rounded-full bg-bg-order-notify text-slate-100"></div>
                     </li>
-                    <li>
-                        <Link to='/cancelled'>Delivery Cancelled</Link>
+                    <li className="flex items-center justify-center gap-1">
+                        <Link to='/cancelled' className="py-2">Delivery Cancelled</Link>  <div className="w-6 text-xs leading-6 text-center rounded-full bg-bg-order-notify text-slate-100"></div>
                     </li>
-                    <li>
-                        <Link to='/delivered'>Delivered</Link>
+                    <li className="flex items-center justify-center gap-1">
+                        <Link to='/delivered' >Delivered</Link>  <div className="w-2 text-xs leading-6 text-center rounded-full bg-bg-order-notify text-slate-100"></div>
                     </li>
                    
                    
